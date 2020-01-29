020c1272b3d6c1aadcc8984d56cb55a48d738138
Me Me Mon <meme@Mes-MacBook-Pro.local>
2020-01-29 01:32:59 -0800

    Updates

---
+++ precache-manifest.ecc89a52eeaac4d2b96547e10c0e7ed9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78472df96799008af010dbcbedff4011",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8bebe3a7d18fa00c4308",
+    "url": "/portfolio/static/css/main.019bb8dd.chunk.css"
+  },
+  {
+    "revision": "0fffb4ae316e09e7d226",
+    "url": "/portfolio/static/js/2.345be7a9.chunk.js"
+  },
+  {
+    "revision": "8bebe3a7d18fa00c4308",
+    "url": "/portfolio/static/js/main.9e5fb55c.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "bed30ddfa5d434e827c775ac9a3b0d38",
+    "url": "/portfolio/static/media/bootstrap.bed30ddf.png"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/portfolio/static/media/css.3f8e68f4.png"
+  },
+  {
+    "revision": "8ed175e35fab343cd57b4963e81f894c",
+    "url": "/portfolio/static/media/engage.8ed175e3.png"
+  },
+  {
+    "revision": "011e11c6a03fede2690d29d7982e9ab0",
+    "url": "/portfolio/static/media/js.011e11c6.png"
+  },
+  {
+    "revision": "e269364c1a2dcb43e94617f7911f611b",
+    "url": "/portfolio/static/media/mac.e269364c.png"
+  },
+  {
+    "revision": "94821cfecbda59d19d109b8493b4c196",
+    "url": "/portfolio/static/media/mysql.94821cfe.png"
+  },
+  {
+    "revision": "654891e1ff48c46145754f2c81787a35",
+    "url": "/portfolio/static/media/php.654891e1.png"
+  },
+  {
+    "revision": "618b081f3dbbccb9d5eb5d7b57c12504",
+    "url": "/portfolio/static/media/profilePic.618b081f.jpg"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/portfolio/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "59b6b8dd6c49e2736ecad1314a4605fc",
+    "url": "/portfolio/static/media/ubuntu.59b6b8dd.png"
+  }
+]);
